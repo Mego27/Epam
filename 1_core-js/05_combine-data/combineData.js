7370fa19fd17e3e5ae521eab772bb05b2aff9de5
Kravchenko Andrew <andrey_kravchenko2@epam.com>
2020-11-13 12:10:21 +0300

    fix: style's errors

--- 1_core-js/05_combine-data/combineData.js
+++ 1_core-js/05_combine-data/combineData.js
@@ -1,61 +1,56 @@
 function combineData(type, ...args) {
-    let result;
-    let data = {};
-    let dataArray = [];
-    let counterDefaultKeys = 1;
-    for(let item of args) {
-      if (item instanceof Array) {
-        item.forEach((value, index) => {
-          data[index] = value;
-          dataArray.push(value);
-        });
-        continue;
-      };
-  
-      if (item instanceof Object) {
-        for(let key in item) {
-          data[key] = item[key];
-          dataArray.push(item[key]);
-        };
-        continue;
-      };
-      data[`key${counterDefaultKeys++}`] = item;
-      dataArray.push(item);
-    };
-  
-    switch (type) {
-      case 'object': {
-        result = data;
-        break;
-      }
-  
-      case 'array': {
-        return dataArray;
-      }
-  
-      case 'number': {
-        for(let value of dataArray) {
-          result += +value;
-        }
-        break;
-      }
-  
-      case 'string': {
-        result = '';
-        for(let value of dataArray) {
-          result += String(value);
-        }
-        break;
-      }
-  
-      case 'boolean': {
-        result = true;
-        for(let value of dataArray) {
-          result = result && value;
-        }
-        break;
-      }
+  let result;
+  let data = {};
+  let dataArray = [];
+  let counterDefaultKeys = 1;
+  args.forEach((item) => {
+    if (item instanceof Array) {
+      item.forEach((value, index) => {
+        data[index] = value;
+        dataArray.push(value);
+      })
+
+      return;
     }
-  
-    return result;
+
+    if (item instanceof Object) {
+      Object.entries(item).forEach(([key, value]) => {
+        data[key] = value;
+        dataArray.push(value);
+      })
+
+      return;
+    }
+    
+    data[`key${counterDefaultKeys++}`] = item;
+    dataArray.push(item);
+  })
+
+  switch (type) {
+    case 'object': {
+      result = data;
+      break;
+    }
+
+    case 'array': {
+      return dataArray;
+    }
+
+    case 'number': {
+      result = dataArray.reduce((result, value) => result + +value, 0);
+      break;
+    }
+
+    case 'string': {
+      result = dataArray.reduce((result, value) => result + value, '');
+      break;
+    }
+
+    case 'boolean': {
+      result = dataArray.reduce((result, value) => result && value, true);
+      break;
+    }
   }
+
+  return result;
+}
