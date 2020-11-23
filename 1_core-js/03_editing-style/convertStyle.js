9861762edadc44fd91df59b81f56020913d977f1
Kravchenko Andrew <andrey_kravchenko2@epam.com>
2020-11-23 12:54:01 +0300

    fix: made more readable assgiment positionTop

--- 1_core-js/03_editing-style/convertStyle.js
+++ 1_core-js/03_editing-style/convertStyle.js
@@ -1,17 +1,15 @@
 function convertStyle({
-  top: positionTop,
+  top,
   bottom: positionBottom,
   margin: {
     top: marginTop = 0,
     bottom: marginBottom = 0,
   },
 }) {
-
   return {
-    positionTop: positionTop * 2,
+    positionTop: top * 2,
     positionBottom: positionBottom * 2,
     marginTop,
     marginBottom,
   };
 }
-  
