613312aaaa86322809473f9589e5322126fed90e
Kravchenko Andrew <andrey_kravchenko2@epam.com>
2020-11-23 12:30:41 +0300

    fix defaulting assigment

--- 1_core-js/01_date-formatting/getDate.js
+++ 1_core-js/01_date-formatting/getDate.js
@@ -1,37 +1,36 @@
 function getDate(day, month, year, format = 'year_month_day') {
-    let date;
-    const isCorrectDay = (typeof(day) === 'number') && (1 <= day <= 31);
-    const isCorrectMonth = (typeof(month) === 'number') && (1 <= month <= 12);
-    const isCorrectYear = (typeof(year) === 'number') && (year >= 1000);
+  let date = null;
+  const isCorrectDay = (typeof (day) === 'number') && (day >= 1 <= 31);
+  const isCorrectMonth = (typeof (month) === 'number') && (month >= 1 <= 12);
+  const isCorrectYear = (typeof (year) === 'number') && (year >= 1000);
 
-    if (isCorrectDay && isCorrectMonth && isCorrectYear) {
-        switch (format) {
-            case 'day.month.year': {
-                date = `${day}.${month}.${year}`;
-                break;
-            }
-                
-            case 'year_month_day': {
-                date = `${year}_${month}_${day}`;
-                break;
-            }
-                
-            case 'object': {
-                date = { day, month, year };
-                break;
-            }
-                
-            case 'iso': {
-                date = new Date(`${day}-${month}-${year}`).toISOString();
-                break;
-            }
-            default: {
-                break;
-            }
-        }
+  if (isCorrectDay && isCorrectMonth && isCorrectYear) {
+    switch (format) {
+      case 'day.month.year': {
+        date = `${day}.${month}.${year}`;
+        break;
+      }
 
-        return date;
+      case 'year_month_day': {
+        date = `${year}_${month}_${day}`;
+        break;
+      }
+
+      case 'object': {
+        date = { day, month, year };
+        break;
+      }
+
+      case 'iso': {
+        date = new Date(`${day}-${month}-${year}`).toISOString();
+        break;
+      }
+
+      default: {
+        break;
+      }
     }
-    
-    return null;
+  }
+
+  return date;
 }
