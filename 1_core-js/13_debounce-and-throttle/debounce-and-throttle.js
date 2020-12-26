a8586f7103f0a06e7b9bc1441af584922e9e9652
Kravchenko Andrew <andrey_kravchenko2@epam.com>
2020-12-26 19:42:51 +0300

    upload debounce-and-throttle.js

---
+++ 1_core-js/13_debounce-and-throttle/debounce-and-throttle.js
@@ -0,0 +1,40 @@
+function debounce(func, delay) {
+  let isCoolDown = false;
+
+  return function debouncedFunc(...args) {
+    if (!isCoolDown) {
+      isCoolDown = true;
+
+      func(...args);
+
+      setTimeout(() => isCoolDown = false, delay);
+    }
+  };
+}
+
+function throttle(func, delay) {
+  let isCoolDown = false;
+  let lastArgs;
+
+  return function throttledFunc(...args) {
+    if (isCoolDown) {
+      lastArgs = args;
+
+      return;
+    }
+
+    isCoolDown = true;
+
+    func.apply(this, lastArgs || args);
+
+    setTimeout(() => {
+      isCoolDown = false;
+
+      if (lastArgs) {
+        throttledFunc(...lastArgs);
+
+        lastArgs = null;
+      }
+    }, delay);
+  };
+}
