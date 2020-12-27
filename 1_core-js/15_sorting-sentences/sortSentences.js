21bf494b8e739836af61cf3bb17a7f2a8ff05bb9
Kravchenko Andrew <andrey_kravchenko2@epam.com>
2020-12-28 00:05:57 +0300

    upload sortSentences.js

---
+++ 1_core-js/15_sorting-sentences/sortSentences.js
@@ -0,0 +1,12 @@
+function compareWords(word1, word2) {
+  return word1.length - word2.length || word1.localeCompare(word2);
+}
+
+function sortSentences(text) {
+  const preparedText = text.replace(/[.,()?!:]/g, '').replace(/ +/g, ' ').trim();
+  const arrayOfWords = preparedText.split(' ');
+
+  arrayOfWords.sort(compareWords);
+
+  return arrayOfWords;
+}
