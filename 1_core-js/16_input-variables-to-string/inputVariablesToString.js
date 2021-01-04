d84c95761e82095026f5f5272656bd6cd623c2c7
Kravchenko Andrew <andrey_kravchenko2@epam.com>
2021-01-04 14:29:23 +0300

    escape {}

--- 1_core-js/16_input-variables-to-string/inputVariablesToString.js
+++ 1_core-js/16_input-variables-to-string/inputVariablesToString.js
@@ -1,3 +1,3 @@
 function inputVariablesToString({ string, ...variables }) {
-  return string.replace(/\${\s*(\w+)\s*}/g, (match, foundVariable) => variables[foundVariable]);
+  return string.replace(/\$\{\s*(\w+)\s*\}/g, (match, foundVariable) => variables[foundVariable]);
 }
