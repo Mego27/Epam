722b9c661fb309104742c8fd8ba2675309505557
Kravchenko Andrew <andrey_kravchenko2@epam.com>
2021-01-04 14:34:28 +0300

    fix arguments of function

--- 1_core-js/16_input-variables-to-string/inputVariablesToString.js
+++ 1_core-js/16_input-variables-to-string/inputVariablesToString.js
@@ -1,3 +1,10 @@
-function inputVariablesToString({ string, ...variables }) {
+function inputVariablesToString(string, variables) {
   return string.replace(/\$\{\s*(\w+)\s*\}/g, (match, foundVariable) => variables[foundVariable]);
 }
+const string = 'I saw ${ a  } mens ${b}.';
+const object = {
+  a: 5,
+  b: 'yesterday',
+  string: 'I saw ${ a  } mens ${b}.',
+};
+console.log(inputVariablesToString(string, object));
