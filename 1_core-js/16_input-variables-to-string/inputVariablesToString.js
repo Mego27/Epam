16f6a9976215c554a1e655f43012f28583d80815
Kravchenko Andrew <andrey_kravchenko2@epam.com>
2021-01-04 14:34:53 +0300

    delete examples for test

--- 1_core-js/16_input-variables-to-string/inputVariablesToString.js
+++ 1_core-js/16_input-variables-to-string/inputVariablesToString.js
@@ -1,10 +1,3 @@
 function inputVariablesToString(string, variables) {
   return string.replace(/\$\{\s*(\w+)\s*\}/g, (match, foundVariable) => variables[foundVariable]);
 }
-const string = 'I saw ${ a  } mens ${b}.';
-const object = {
-  a: 5,
-  b: 'yesterday',
-  string: 'I saw ${ a  } mens ${b}.',
-};
-console.log(inputVariablesToString(string, object));
