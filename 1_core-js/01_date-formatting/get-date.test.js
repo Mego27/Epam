8bcc93ee2514c252f3b762bd304b580a938683fc
Kravchenko Andrew <andrey_kravchenko2@epam.com>
2021-01-23 12:44:56 +0300

    fix: to jest

--- 1_core-js/01_date-formatting/get-date.test.js
+++ 1_core-js/01_date-formatting/get-date.test.js
@@ -1,18 +1,17 @@
-const assert = require('assert');
 const getDate = require('./get-date');
 
 describe('getDate', () => {
   describe('for 1st January 2021', () => {
     it('should return 2021_1_1 when format not entered', () => {
-      assert.strictEqual(getDate(1, 1, 2021), '2021_1_1');
+      expect(getDate(1, 1, 2021)).toBe('2021_1_1');
     });
 
     it('should return 1.1.2021 when format day.month.year', () => {
-      assert.strictEqual(getDate(1, 1, 2021, 'day.month.year'), '1.1.2021');
+      expect(getDate(1, 1, 2021, 'day.month.year')).toBe('1.1.2021');
     });
 
     it('should return 2021_1_1 when format year_month_day', () => {
-      assert.strictEqual(getDate(1, 1, 2021, 'year_month_day'), '2021_1_1');
+      expect(getDate(1, 1, 2021, 'year_month_day')).toBe('2021_1_1');
     });
 
     it('should return object when format object', () => {
@@ -21,17 +20,17 @@ describe('getDate', () => {
         month: 1,
         year: 2021,
       };
-      assert.deepStrictEqual(getDate(1, 1, 2021, 'object'), testingDate);
+      expect(getDate(1, 1, 2021, 'object')).toStrictEqual(testingDate);
     });
 
     it('should return 2020-12-31T21:00:00.000Z when format iso', () => {
-      assert.strictEqual(getDate(1, 1, 2021, 'iso'), '2020-12-31T21:00:00.000Z');
+      expect(getDate(1, 1, 2021, 'iso')).toBe('2020-12-31T21:00:00.000Z');
     });
 
     it('should return null when day|month|year incorrect', () => {
-      assert.strictEqual(getDate(90, 1, 2021), null);
-      assert.strictEqual(getDate(1, 34, 2021), null);
-      assert.strictEqual(getDate(1, 1, 201), null);
+      expect(getDate(90, 1, 2021)).toBe(null);
+      expect(getDate(1, 34, 2021)).toBe(null);
+      expect(getDate(1, 1, 201)).toBe(null);
     });
   });
 });
