ac02868ccc6e727551d1cce9e67f35f507cd3a79
Kravchenko Andrew <andrey_kravchenko2@epam.com>
2021-01-26 21:31:08 +0300

    fix: rename debounce to throttle & create new throttle

--- 1_core-js/13_debounce-and-throttle/debounce-and-throttle.js
+++ 1_core-js/13_debounce-and-throttle/debounce-and-throttle.js
@@ -1,44 +1,22 @@
 function debounce(func, delay) {
-  let isCoolDown = false;
+  let timeout;
 
   return function debouncedFunc(...args) {
-    if (!isCoolDown) {
-      isCoolDown = true;
-
-      setTimeout(() => {
-        isCoolDown = false;
-        func.apply(this, args);
-      }, delay);
-    }
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, args), delay);
   };
 }
 
 function throttle(func, delay) {
   let isCoolDown = false;
-  let lastArgs;
-  let savedThis;
 
   return function throttledFunc(...args) {
-    if (isCoolDown) {
-      lastArgs = args;
-      savedThis = this;
+    if (!isCoolDown) {
+      isCoolDown = true;
 
-      return;
+      func.apply(this, args);
+
+      setTimeout(() => (isCoolDown = false), delay);
     }
-
-    isCoolDown = true;
-
-    func.apply(this, lastArgs || args);
-
-    setTimeout(() => {
-      isCoolDown = false;
-
-      if (lastArgs) {
-        throttledFunc.apply(savedThis, lastArgs);
-
-        lastArgs = null;
-        savedThis = null;
-      }
-    }, delay);
   };
 }
