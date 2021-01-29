89c9a8358f00dac60538d3c5dc533a205b964f6e
Kravchenko Andrew <andrey_kravchenko2@epam.com>
2021-01-29 18:08:19 +0300

    fix: delete comments :)

--- 1_core-js/14_manager-of-messages/manager-of-messages.js
+++ 1_core-js/14_manager-of-messages/manager-of-messages.js
@@ -46,6 +46,7 @@ function ManagerOfMessages() {
 
     function print(textOfMessage, type, color, isPrintDate) {
       const fullText = isPrintDate ? `${new Date().toLocaleTimeString()}: ${type}(${color}): ${textOfMessage}` : `${type}(${color}): ${textOfMessage}`;
+
       console.log(fullText);
     }
 
@@ -59,59 +60,14 @@ function ManagerOfMessages() {
       }
     }
 
-    // const type = userType || this._defineType(text);
     const type = defineType(text);
-    // const color = this._selectColorByType(type);
     const color = selectColorByType(type);
     const { shouldPrintDate: isPrintDate } = this.properties;
 
     if (this.properties.delay > 0) {
-      // this._printWithDelay(text, type, color, isPrintDate);
       printWithDelay(text, type, color, isPrintDate);
     } else {
-      // this._print(text, type, color, isPrintDate);
       print(text, type, color, isPrintDate);
     }
   };
-
-  // this._printWithDelay = function debounce(text, type, color, isPrintDate) {
-  //   if (!this.properties.isCoolDown) {
-  //     this.properties.isCoolDown = true;
-
-  //     this._print(text, type, color, isPrintDate);
-
-  //     setTimeout(() => (this.properties.isCoolDown = false), this.properties.delay);
-  //   }
-  // };
-
-  // this._print = function printFunc(text, type, color, isPrintDate) {
-  //   const fullText = isPrintDate ? `${new Date().toLocaleTimeString()}: ${type}(${color}): ${text}` : `${type}(${color}): ${text}`;
-  //   console.log(fullText);
-  // };
-
-  // this._defineType = (text) => {
-  //   let resultType = 'information';
-  //   Object.entries(this.properties.keywordOfType).forEach(([type, word]) => {
-  //     if (text.toLowerCase().includes(word)) {
-  //       resultType = type;
-  //     }
-  //   });
-
-  //   return resultType;
-  // };
-
-  // this._selectColorByType = (type) => {
-  //   const entries = Object.entries(this.properties.color).filter(([key, value]) => key === type);
-  //   const color = entries[0][1];
-
-  //   return color;
-  // };
 }
-
-const managerOfMessages = new ManagerOfMessages();
-managerOfMessages.properties.shouldPrintDate = true;
-// managerOfMessages.properties.delay = 5000;
-managerOfMessages.printMessage('printing sad', 'error');
-managerOfMessages.printMessage('printing wwevecrwc предупреждение');
-// console.log(managerOfMessages._selectColorByType('information'));
-setTimeout(() => managerOfMessages.printMessage('printing 12413255 ошибка', ''), 2500);
