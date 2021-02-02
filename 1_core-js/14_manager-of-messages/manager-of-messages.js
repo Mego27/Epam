efaf19d72c64f6e27704e2924495310f0a0420c2
Kravchenko Andrew <andrey_kravchenko2@epam.com>
2021-02-02 22:06:08 +0300

    fix: move funcs outside func-constructor & add initialization properties

--- 1_core-js/14_manager-of-messages/manager-of-messages.js
+++ 1_core-js/14_manager-of-messages/manager-of-messages.js
@@ -1,17 +1,56 @@
-function ManagerOfMessages() {
+function defineType(properties, textOfMessage) {
+  let resultType = 'information';
+  Object.entries(properties.keywordOfType).forEach(([type, word]) => {
+    if (textOfMessage.toLowerCase().includes(word)) {
+      resultType = type;
+    }
+  });
+
+  return resultType;
+}
+
+function selectColorByType(properties, type) {
+  const entries = Object.entries(properties.color).filter(([key, value]) => key === type);
+  const color = entries[0][1];
+
+  return color;
+}
+
+function print(textOfMessage, type, color, isPrintDate) {
+  const fullText = isPrintDate ? `${new Date().toLocaleTimeString()}: ${type}(${color}): ${textOfMessage}` : `${type}(${color}): ${textOfMessage}`;
+
+  console.log(fullText);
+}
+
+function printWithDelay(properties, textOfMessage, type, color, isPrintDate) {
+  if (!properties.isCoolDown) {
+    properties.isCoolDown = true;
+
+    print(textOfMessage, type, color, isPrintDate);
+
+    setTimeout(() => (properties.isCoolDown = false), properties.delay);
+  }
+}
+
+function ManagerOfMessages(
+  { informationColor, warningColor, errorColor },
+  { informationKeyword, warningKeyword, errorKeyword },
+  shouldPrintDate,
+  delay,
+) {
   this.properties = {
-    shouldPrintDate: false,
+    shouldPrintDate,
     color: {
-      information: 'white',
-      warning: 'yellow',
-      error: 'red',
+      information: informationColor,
+      warning: warningColor,
+      error: errorColor,
     },
     keywordOfType: {
-      information: 'информация',
-      warning: 'предупреждение',
-      error: 'ошибка',
+      information: informationKeyword,
+      warning: warningKeyword,
+      error: errorKeyword,
     },
-    delay: 1000,
+    delay,
     isCoolDown: false,
   };
 
@@ -25,47 +64,12 @@ function ManagerOfMessages() {
 
   this.printMessage = function printMessage(text) {
     const { properties } = this;
-
-    function defineType(textOfMessage) {
-      let resultType = 'information';
-      Object.entries(properties.keywordOfType).forEach(([type, word]) => {
-        if (textOfMessage.toLowerCase().includes(word)) {
-          resultType = type;
-        }
-      });
-
-      return resultType;
-    }
-
-    function selectColorByType(type) {
-      const entries = Object.entries(properties.color).filter(([key, value]) => key === type);
-      const color = entries[0][1];
-
-      return color;
-    }
-
-    function print(textOfMessage, type, color, isPrintDate) {
-      const fullText = isPrintDate ? `${new Date().toLocaleTimeString()}: ${type}(${color}): ${textOfMessage}` : `${type}(${color}): ${textOfMessage}`;
-
-      console.log(fullText);
-    }
-
-    function printWithDelay(textOfMessage, type, color, isPrintDate) {
-      if (!properties.isCoolDown) {
-        properties.isCoolDown = true;
-
-        print(textOfMessage, type, color, isPrintDate);
-
-        setTimeout(() => (properties.isCoolDown = false), properties.delay);
-      }
-    }
-
-    const type = defineType(text);
-    const color = selectColorByType(type);
-    const { shouldPrintDate: isPrintDate } = this.properties;
+    const type = defineType(properties, text);
+    const color = selectColorByType(properties, type);
+    const { shouldPrintDate: isPrintDate } = properties;
 
     if (this.properties.delay > 0) {
-      printWithDelay(text, type, color, isPrintDate);
+      printWithDelay(properties, text, type, color, isPrintDate);
     } else {
       print(text, type, color, isPrintDate);
     }
