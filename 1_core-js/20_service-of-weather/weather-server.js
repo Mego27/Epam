401c0246e69be57c73de60f0182e9da6da2d4f77
Kravchenko Andrew <andrey_kravchenko2@epam.com>
2021-02-20 17:48:46 +0300

    fix: change array cities element array to element object

--- 1_core-js/20_service-of-weather/weather-server.js
+++ 1_core-js/20_service-of-weather/weather-server.js
@@ -1,28 +1,76 @@
 class WeatherServer {
   constructor() {
     this.cities = [
-      ['Кампала', 'Уганда', 0],
-      ['Пхукет', 'Тайланд', 8],
-      ['Санто-Доминго', 'Доминикана', 18],
-      ['Дубай', 'Арабские Эмираты', 25],
-      ['Шанхай', 'Китай', 31],
-      ['Лос-Анжелес', 'США', 34],
-      ['Мадрид', 'Испания', 40],
-      ['Париж', 'Франция', 49],
-      ['Лондон', 'Великобритания', 52],
-      ['Москва', 'Россия', 56],
-      ['Осло', 'Норвегия', 60],
-      ['Мурманск', 'Россия', 69],
+      {
+        city: 'Кампала',
+        country: 'Уганда',
+        latitude: 0,
+      },
+      {
+        city: 'Пхукет',
+        country: 'Тайланд',
+        latitude: 8,
+      },
+      {
+        city: 'Санто-Доминго',
+        country: 'Доминикана',
+        latitude: 18,
+      },
+      {
+        city: 'Дубай',
+        country: 'Арабские Эмираты',
+        latitude: 25,
+      },
+      {
+        city: 'Шанхай',
+        country: 'Китай',
+        latitude: 31,
+      },
+      {
+        city: 'Лос-Анжелес',
+        country: 'США',
+        latitude: 34,
+      },
+      {
+        city: 'Мадрид',
+        country: 'Испания',
+        latitude: 40,
+      },
+      {
+        city: 'Париж',
+        country: 'Франция',
+        latitude: 49,
+      },
+      {
+        city: 'Лондон',
+        country: 'Великобритания',
+        latitude: 52,
+      },
+      {
+        city: 'Москва',
+        country: 'Россия',
+        latitude: 56,
+      },
+      {
+        city: 'Осло',
+        country: 'Норвегия',
+        latitude: 60,
+      },
+      {
+        city: 'Мурманск',
+        country: 'Россия',
+        latitude: 69,
+      },
     ];
   }
 
   getCities() {
-    return this.cities.map(([city, country, latitude]) => city);
+    return this.cities.map(({ city }) => city);
   }
 
   getAvgTemperatureOfCity(userCity, dayYear) {
     try {
-      const infoCity = this.cities.find(([city]) => city === userCity);
+      const infoCity = this.cities.find(({ city }) => city === userCity);
 
       if (infoCity === undefined) {
         throw new Error('Данного города нет в базе данных!');
@@ -36,7 +84,6 @@ class WeatherServer {
 
       return (30 + latitude * ((182 - (202 - dayYear)) / 210 - 1)).toFixed(2);
     } catch (error) {
-      // console.log(error.message);
       return error;
     }
   }
