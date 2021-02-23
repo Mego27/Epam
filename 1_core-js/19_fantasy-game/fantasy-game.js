5adbd847ec00cac9f0dfea3db2539ff3ca31577f
Kravchenko Andrew <andrey_kravchenko2@epam.com>
2021-02-23 13:08:03 +0300

    fix: delete extra parentheses & create parent class Entity & use error first pattern

--- 1_core-js/19_fantasy-game/fantasy-game.js
+++ 1_core-js/19_fantasy-game/fantasy-game.js
@@ -1,19 +1,24 @@
 function getRandomIntInRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 const arena = [];
 
-class Warrior {
+class Entity {
   constructor(name, arena) {
     this.name = name;
-    this.type = 'warrior';
     this.healthPoints = getRandomIntInRange(10, 100);
-    this.minAttackDamage = getRandomIntInRange(1, 20);
-    this.maxAttackDamage = getRandomIntInRange(20, 40);
-    this.isAttackMonster = false;
+    this.minActionPower = getRandomIntInRange(1, 20);
+    this.maxActionPower = getRandomIntInRange(20, 40);
 
     arena.push(this);
+  }
+}
+class Warrior extends Entity {
+  constructor(name, arena) {
+    super(name, arena);
+    this.type = 'warrior';
+    this.isAttackMonster = false;
   }
 
   attack(enemyName) {
@@ -25,40 +30,38 @@ class Warrior {
       return;
     }
 
-    const damage = getRandomIntInRange(this.minAttackDamage, this.maxAttackDamage);
     const enemy = arena.find((entry) => entry.name === enemyName);
 
-    if (enemy !== undefined) {
-      enemy.healthPoints -= damage;
-
-      const amountOfDamageMessage = `${this.name} нанёс ${damage} урона ${enemyName}`;
-      const remainingHealthMessage = `У ${enemyName} осталось ${enemy.healthPoints} единиц здоровья`;
-
-      console.log(amountOfDamageMessage);
-      console.log(remainingHealthMessage);
-
-      if (enemy.type === 'monster') {
-        this.isAttackMonster = true;
-
-        enemy.attack(this, 'attack');
-      }
-    } else {
+    if (enemy === undefined) {
       const searchingErrorMessage = 'Такой цели не найдено!';
 
       console.log(searchingErrorMessage);
+
+      return;
+    }
+
+    const damage = getRandomIntInRange(this.minActionPower, this.maxActionPower);
+
+    enemy.healthPoints -= damage;
+
+    const amountOfDamageMessage = `${this.name} нанёс ${damage} урона ${enemyName}`;
+    const remainingHealthMessage = `У ${enemyName} осталось ${enemy.healthPoints} единиц здоровья`;
+
+    console.log(amountOfDamageMessage);
+    console.log(remainingHealthMessage);
+
+    if (enemy.type === 'monster') {
+      this.isAttackMonster = true;
+
+      enemy.attack(this, 'attack');
     }
   }
 }
 
-class Healer {
+class Healer extends Entity {
   constructor(name, arena) {
-    this.name = name;
+    super(name, arena);
     this.type = 'healer';
-    this.healthPoints = getRandomIntInRange(10, 100);
-    this.minHealingPower = getRandomIntInRange(1, 20);
-    this.maxHealingPower = getRandomIntInRange(20, 40);
-
-    arena.push(this);
   }
 
   heal(targetName) {
@@ -70,51 +73,49 @@ class Healer {
       return;
     }
 
-    const healingPower = getRandomIntInRange(this.minHealingPower, this.maxHealingPower);
     const target = arena.find((entry) => entry.name === targetName);
 
-    if (target !== undefined) {
-      target.healthPoints += healingPower;
-
-      const amountOfHealthMessage = `${this.name} вылечил на ${healingPower} единиц здоровья ${targetName}`;
-      const remainingHealthMessage = `У ${targetName} стало ${target.healthPoints} единиц здоровья`;
-
-      console.log(amountOfHealthMessage);
-      console.log(remainingHealthMessage);
-
-      if (target.type === 'monster') {
-        target.attack(this, 'heal');
-      } else if ((target.type === 'warrior') && target.isAttackMonster) {
-        const monsters = arena
-          .filter((entry) => (entry.type === 'monster') && (entry.healthPoints > 0));
-        const randomMonsterIndex = getRandomIntInRange(0, monsters.length - 1);
-        const monster = monsters[randomMonsterIndex];
-
-        if (monster !== undefined) {
-          monster.attack(this, 'healEnemy');
-        } else {
-          const searchingMonstersErrorMessage = 'Живых монстров не осталось';
-
-          console.log(searchingMonstersErrorMessage);
-        }
-      }
-    } else {
+    if (target === undefined) {
       const searchingErrorMessage = 'Такой цели не найдено!';
 
       console.log(searchingErrorMessage);
+
+      return;
+    }
+
+    const healingPower = getRandomIntInRange(this.minActionPower, this.maxActionPower);
+
+    target.healthPoints += healingPower;
+
+    const amountOfHealthMessage = `${this.name} вылечил на ${healingPower} единиц здоровья ${targetName}`;
+    const remainingHealthMessage = `У ${targetName} стало ${target.healthPoints} единиц здоровья`;
+
+    console.log(amountOfHealthMessage);
+    console.log(remainingHealthMessage);
+
+    if (target.type === 'monster') {
+      target.attack(this, 'heal');
+    } else if (target.type === 'warrior' && target.isAttackMonster) {
+      const monsters = arena
+        .filter((entry) => (entry.type === 'monster') && (entry.healthPoints > 0));
+      const randomMonsterIndex = getRandomIntInRange(0, monsters.length - 1);
+      const monster = monsters[randomMonsterIndex];
+
+      if (monster !== undefined) {
+        monster.attack(this, 'healEnemy');
+      } else {
+        const searchingMonstersErrorMessage = 'Живых монстров не осталось';
+
+        console.log(searchingMonstersErrorMessage);
+      }
     }
   }
 }
 
-class Monster {
+class Monster extends Entity {
   constructor(name, arena) {
-    this.name = name;
+    super(name, arena);
     this.type = 'monster';
-    this.healthPoints = getRandomIntInRange(10, 100);
-    this.minAttackDamage = getRandomIntInRange(1, 20);
-    this.maxAttackDamage = getRandomIntInRange(20, 40);
-
-    arena.push(this);
   }
 
   attack(target, actionOfTarget) {
@@ -126,11 +127,11 @@ class Monster {
       return;
     }
 
-    const damage = getRandomIntInRange(this.minAttackDamage, this.maxAttackDamage);
+    const damage = getRandomIntInRange(this.minActionPower, this.maxActionPower);
 
     let enemy;
 
-    if ((actionOfTarget === 'attack') || (actionOfTarget === 'healEnemy')) {
+    if (actionOfTarget === 'attack' || actionOfTarget === 'healEnemy') {
       enemy = target;
     } else {
       const aviableToAttackEntries = arena
